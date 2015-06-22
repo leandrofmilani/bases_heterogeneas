6af2fc10cc7afaa83aab7e813909743027392167
josemarfuzinatto <josemarfuzinatto@hotmail.com>
2015-06-22 00:52:36 -0300

    removido arquivos nao usados

--- conectaPostGreSQL.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  // Conecta ao Servidor PostGreSQL
-// Cria a conexão
-$conexaoPSQL = pg_connect("host=localhost dbname=banco_g2 user=postgres password=postgres") or die ("Não foi possível conectar ao Banco de dados.");
-
-?>
